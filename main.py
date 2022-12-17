c666517d73cae9fdfc85d9bbb48810a573f20d59
Nikolay <nikolka.007@yandex.ru>
2022-12-17 12:58:33 +0300

    Change welcome function

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def choice_char_class():
 
 
 def main():
-    print('Приветствую тебя, искатель приключений!')
+    print('Приветствую тебя, человек')
     print('Прежде чем начать игру...')
     char_name = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
